f349c44851c03f0c9ec927306c8a2650a0b5ba3b
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-08-02 16:17:42 -0400

    used reducer

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {...state, friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+      let array = state.friends
+      let newArray = array.filter(x => !(x.id === action.id))
+      return {...state, friends: newArray}
+    default:
+    return state
+  }
+
 }
